33e4ed96075b3724f458743b2faf8f01ce05b8b9
Stiven Parada <barloff2@gmail.com>
2022-11-24 16:10:34 -0500

    Inicio IDE

--- src/main/java/co/com/inspirar/DAO/EmpresaDAO.java
+++
@@ -1,9 +0,0 @@
-package co.com.inspirar.DAO;
-
-import java.util.List;
-import co.com.inspirar.modelo.Empresa;
-public interface EmpresaDAO {
-    public List<Empresa> mostrarEmpresa() throws Exception;
-    public void crearEmpresa() throws Exception;
-    public void editarEmpresa() throws Exception;
-}
